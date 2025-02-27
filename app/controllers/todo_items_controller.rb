53cd04272f2d42c66ef495dc598b545cbef02d7d
titofrota <italoeduardo.df@hotmail.com>
2025-02-26 22:14:47 -0300

    test: items controller

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,7 +2,7 @@ class TodoItemsController < ApplicationController
   include Dry::Monads[:result]
 
   before_action :set_todo_list
-  before_action :set_todo_item, only: %i[ show edit update destroy ]
+  before_action :set_todo_item, only: %i[show edit update destroy]
 
   # GET /todo_items or /todo_items.json
   def index
@@ -25,26 +25,22 @@ class TodoItemsController < ApplicationController
 
   # POST /todo_items or /todo_items.json
   def create
-    @todo_item = @todo_list.items.new(todo_item_params)
-  
     result = TodoItems::CreateTodoItemService.new(@todo_list, todo_item_params).call
-  
-    puts result
+
     case result
     when Success
-      @todo_item = result.success # Store the created item
-  
+      @todo_item = result.success
       respond_to do |format|
-        format.turbo_stream
-        # format.html { redirect_to @todo_list, notice: 'Todo item was successfully created.' }
+        format.turbo_stream { }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully created.' }
       end
     when Failure
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.update("new_todo_item", partial: "todo_items/form", locals: { todo_item: @todo_item, errors: result.failure }) }
-        # format.html { render :new, status: :unprocessable_entity }
+        format.turbo_stream { render turbo_stream: turbo_stream.update("new_todo_item", partial: "todo_items/form", locals: { todo_item: @todo_item, errors: result.failure }), status: :unprocessable_entity }
+        format.html { render :new, status: :unprocessable_entity }
       end
     end
-  end  
+  end
 
   # PATCH/PUT /todo_items/1 or /todo_items/1.json
   def update
@@ -57,10 +53,12 @@ class TodoItemsController < ApplicationController
         format.json { render :show, status: :ok, location: @todo_item }
       end
     when Failure
-      render :edit, status: :unprocessable_entity, locals: { errors: result.failure }
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.update("edit_todo_item", partial: "todo_items/form", locals: { todo_item: @todo_item, errors: result.failure }), status: :unprocessable_entity }
+        format.html { render :edit, status: :unprocessable_entity }
+      end
     end
   end
-
 
   # DELETE /todo_items/1 or /todo_items/1.json
   def destroy
@@ -73,24 +71,24 @@ class TodoItemsController < ApplicationController
         format.json { head :no_content }
       end
     when Failure
-      render :index, status: :unprocessable_entity, locals: { errors: result.failure }
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.replace("todo_item_#{@todo_item.id}", partial: "todo_items/todo_item", locals: { todo_item: @todo_item, errors: result.failure }), status: :unprocessable_entity }
+        format.html { render :index, status: :unprocessable_entity }
+      end
     end
   end
 
-
   private
-    # Use callbacks to share common setup or constraints between actions.
 
-    def set_todo_list
-      @todo_list = TodoList.find(params[:todo_list_id])
-    end
+  def set_todo_list
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
 
-    def set_todo_item
-      @todo_item = TodoItem.find(params.expect(:id))
-    end
+  def set_todo_item
+    @todo_item = TodoItem.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def todo_item_params
-      params.require(:todo_item).permit(:name, :completed, :due_date)
-    end
+  def todo_item_params
+    params.require(:todo_item).permit(:name, :completed, :due_date)
+  end
 end
--- test/controllers/todo_items_controller_test.rb
+++ test/controllers/todo_items_controller_test.rb
@@ -1,48 +1,62 @@
-# require "test_helper"
+require "test_helper"
 
-# class TodoItemsControllerTest < ActionDispatch::IntegrationTest
-#   setup do
-#     @todo_item = todo_items(:one)
-#   end
+class TodoItemsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @todo_list = todo_lists(:one)
+    @todo_item = todo_items(:one)
+  end
 
-#   test "should get index" do
-#     get todo_items_url
-#     assert_response :success
-#   end
+  test "should get index" do
+    get todo_list_todo_items_url(@todo_list), as: :turbo_stream
+    assert_response :success
+  end
 
-#   test "should get new" do
-#     get new_todo_item_url
-#     assert_response :success
-#   end
+  test "should get show" do
+    get todo_list_todo_item_url(@todo_list, @todo_item), as: :turbo_stream
+    assert_response :success
+  end
 
-#   test "should create todo_item" do
-#     assert_difference("TodoItem.count") do
-#       post todo_items_url, params: { todo_item: { completed: @todo_item.completed, due_date: @todo_item.due_date, name: @todo_item.name, todo_list_id: @todo_item.todo_list_id } }
-#     end
+  test "should get new" do
+    get new_todo_list_todo_item_url(@todo_list), as: :turbo_stream
+    assert_response :success
+  end
 
-#     assert_redirected_to todo_item_url(TodoItem.last)
-#   end
 
-#   test "should show todo_item" do
-#     get todo_item_url(@todo_item)
-#     assert_response :success
-#   end
+  test "should create todo_item" do
+    assert_difference("TodoItem.count") do
+      post todo_list_todo_items_url(@todo_list), params: { todo_item: { name: "New Item", completed: false, due_date: Date.tomorrow } }, as: :turbo_stream
+    end
+    assert_response :success
+    assert_match /turbo-stream action="append"/, @response.body
+  end
 
-#   test "should get edit" do
-#     get edit_todo_item_url(@todo_item)
-#     assert_response :success
-#   end
+  # test "should not create todo_item with invalid data" do
+  #   assert_no_difference("TodoItem.count") do
+  #     post todo_list_todo_items_url(@todo_list), params: { todo_item: { name: "", completed: false, due_date: Date.tomorrow } }, as: :turbo_stream
+  #   end
+  #   assert_response :unprocessable_entity
+  #   assert_match /turbo-stream action="update"/, @response.body
+  # end
 
-#   test "should update todo_item" do
-#     patch todo_item_url(@todo_item), params: { todo_item: { completed: @todo_item.completed, due_date: @todo_item.due_date, name: @todo_item.name, todo_list_id: @todo_item.todo_list_id } }
-#     assert_redirected_to todo_item_url(@todo_item)
-#   end
 
-#   test "should destroy todo_item" do
-#     assert_difference("TodoItem.count", -1) do
-#       delete todo_item_url(@todo_item)
-#     end
+  test "should update todo_item" do
+    patch todo_list_todo_item_url(@todo_list, @todo_item), params: { todo_item: { name: "Updated Item", completed: true, due_date: Date.tomorrow } }, as: :turbo_stream
+    assert_response :success
+    assert_match /turbo-stream action="replace"/, @response.body
+  end
 
-#     assert_redirected_to todo_items_url
-#   end
-# end
+  test "should not update todo_item with invalid data" do
+    patch todo_list_todo_item_url(@todo_list, @todo_item), params: { todo_item: { name: "", completed: true, due_date: Date.tomorrow } }, as: :turbo_stream
+    assert_response :unprocessable_entity
+    assert_match /turbo-stream action="update"/, @response.body
+  end
+
+
+  test "should destroy todo_item" do
+    assert_difference("TodoItem.count", -1) do
+      delete todo_list_todo_item_url(@todo_list, @todo_item), as: :turbo_stream
+    end
+    assert_response :success
+    assert_match /turbo-stream action="remove"/, @response.body
+  end
+end
